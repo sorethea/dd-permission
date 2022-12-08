68dc92f81b29050e0c4b3bc77c56b02e9f733ee4
Sorethea OK <sorethea@gmail.com>
2022-12-08 09:51:03 +0700

    RoleResource.php

--- src/PermissionServiceProvider.php
+++ src/PermissionServiceProvider.php
@@ -4,12 +4,14 @@ namespace Sorethea\DdPermission;
 
 use Filament\PluginServiceProvider;
 use Sorethea\DdPermission\Filament\Resources\PermissionResource;
+use Sorethea\DdPermission\Filament\Resources\RoleResource;
 use Spatie\LaravelPackageTools\Package;
 
 class PermissionServiceProvider extends PluginServiceProvider
 {
     protected array $resources =[
         PermissionResource::class,
+        RoleResource::class,
     ];
     public function configurePackage(Package $package): void
     {
