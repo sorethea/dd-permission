cfd04eb16cdab3e4216f27a9edbac4f6d4061d3c
Sorethea OK <sorethea@gmail.com>
2022-12-08 09:28:52 +0700

    .\PermissionResource

--- src/PermissionServiceProvider.php
+++ src/PermissionServiceProvider.php
@@ -14,6 +14,7 @@ class PermissionServiceProvider extends PluginServiceProvider
     public function configurePackage(Package $package): void
     {
         $package->name("dd-permission")
+            ->hasTranslations()
             ->hasConfigFile("dd-permission");
     }
 }
