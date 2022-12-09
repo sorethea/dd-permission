915c6d5003762ff0147ebac260477baa96f549f4
Sorethea OK <sorethea@gmail.com>
2022-12-09 09:19:05 +0700

    PermissionSeeder.php

--- src/Filament/Resources/PermissionResource.php
+++ src/Filament/Resources/PermissionResource.php
@@ -34,7 +34,7 @@ class PermissionResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make("name")->searchable()->sortable(),
-                Tables\Columns\TextColumn::make("roles")->counts(),
+                Tables\Columns\TextColumn::make("roles")->counts('roles'),
             ])
             ->filters([
                 //
