2f050928f7504bb83c25ea6306576a9cef13ad7b
Sorethea OK <sorethea@gmail.com>
2022-12-09 15:07:02 +0700

    AuthServiceProvider.php

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace Sorethea\DdPermission;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Sorethea\DdPermission\Policies\PermissionPolicy;
 use Sorethea\DdPermission\Policies\RolePolicy;
 use Spatie\Permission\Models\Permission;
@@ -10,10 +10,9 @@ use Spatie\Permission\Models\Role;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    protected array $policies = [
+    protected $policies = [
         Role::class => RolePolicy::class,
         Permission::class => PermissionPolicy::class
-
     ];
 
     public function boot()
