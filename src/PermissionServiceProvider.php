33acfc51bdde37ae58f9971a5d6a1e96cc33bda9
Sorethea OK <sorethea@gmail.com>
2022-12-09 14:55:33 +0700

    PermissionSeeder.php

--- src/PermissionServiceProvider.php
+++ src/PermissionServiceProvider.php
@@ -38,6 +38,8 @@ class PermissionServiceProvider extends PluginServiceProvider
             ],"dd-permission-seeds");
         }
 
-        $this->registerPolicies();
+        //$this->registerPolicies();
     }
+
+
 }
