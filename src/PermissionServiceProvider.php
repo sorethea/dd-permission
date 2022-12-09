0ef5a1c0984b95385bac80eebaf0c2bc2d9d59b7
Sorethea OK <sorethea@gmail.com>
2022-12-09 10:19:11 +0700

    PermissionSeeder.php

--- src/PermissionServiceProvider.php
+++ src/PermissionServiceProvider.php
@@ -9,27 +9,23 @@ use Spatie\LaravelPackageTools\Package;
 
 class PermissionServiceProvider extends PluginServiceProvider
 {
-    protected function publishes(array $paths, $groups = null)
-    {
-        $this->publishes([__DIR__ . '/../database/seeds/PermissionSeeder.php' => database_path("seeders/PermissionSeeder.php")], "dd-permission-seeds");
-    }
 
-    protected array $resources = [
+    protected array $resources =[
         PermissionResource::class,
         RoleResource::class,
     ];
-
     public function configurePackage(Package $package): void
     {
         $package->name("dd-permission")
             ->hasTranslations()
             ->hasConfigFile("dd-permission");
     }
-
-//    public function boot() :void
-//    {
-//        if($this->app->runningInConsole()){
-//            $this->publishes();
-//        }
-//    }
+    public function packageBooted(): void
+    {
+        if($this->app->runningInConsole()){
+            $this->publishes([
+                __DIR__.'/../database/seeds/PermissionSeeder.php'=>database_path("seeders/PermissionSeeder.php")
+            ],"dd-permission-seeds");
+        }
+    }
 }
